eb30fc6408c9b71fe1848b4999959a20eb8b2421
seungjun0423 <sibonum@gmail.com>
2023-02-25 16:20:03 +0900

    eslint 설정 수정

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,18 +1,54 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     },
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['unused-imports'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts']
+      }
     },
-    "plugins": [
-        "@typescript-eslint"
+    'import/extensions': ['.js', '.ts']
+  },
+  ignorePatterns: ['build/', 'node_modules/', '.pnp.cjs', '.yarn/'],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never'
+      }
     ],
-    "rules": {
-    }
-}
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error']
+      }
+    ],
+    'import/no-unresolved': 'off',
+    'no-use-before-define': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ],
+    'comma-dangle': ['error', 'never']
+  }
+};
