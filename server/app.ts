ac77ef048803725217d99e86f4a270fde390d20a
seungjun0423 <sibonum@gmail.com>
2023-02-25 16:13:20 +0900

    로그 수정

--- server/app.ts
+++ server/app.ts
@@ -3,10 +3,10 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('Workin on Now!')
   res.send('Hello World!');
 });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+  console.log('Workin on Now!')
 });
