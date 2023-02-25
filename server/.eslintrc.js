45aee10e3dd24275a3798b0780b6a51060106103
seungjun0423 <sibonum@gmail.com>
2023-02-25 21:58:58 +0900

    에러 수정

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:recommended','airbnb'],
+  extends: ['airbnb', "prettier"],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
