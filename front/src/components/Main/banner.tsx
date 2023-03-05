29b69b397e996adf348eb3b2c909178a9543b1ac
junhyungkwon <aofry@naver.com>
2023-03-05 23:16:11 +0900

    [FE-banner] modifi: banner UI

--- front/src/components/Main/banner.tsx
+++ front/src/components/Main/banner.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 function Banner() {
   return (
     <div className="banner">
-      <h1>banner</h1>
+      <div className="banner_set">
+        <div className="banner1">펀딩 중</div>
+        <div className="banner1">펀딩 성공</div>
+      </div>
     </div>
   );
 }
--- front/src/components/Main/index.tsx
+++ front/src/components/Main/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from './banner';
 import Funding from './funding';
 import Navbar from './navbar';
+import Bannerimg from './bannerimg';
 
 function Main() {
   return (
     <div className="index">
       <Navbar />
       <Banner />
+      <Bannerimg />
       <Funding />
     </div>
   );
