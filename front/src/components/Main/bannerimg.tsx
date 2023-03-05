b01c3fd04bd62fdc7e2858c5004fcf1e143e3a92
junhyungkwon <aofry@naver.com>
2023-03-05 23:17:51 +0900

    [FE-banner] banner_img Folder Create

---
+++ front/src/components/Main/bannerimg.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Bannerimg() {
+  return (
+    <div className="bannerimg">
+      <div className="banner_img">img</div>
+    </div>
+  );
+}
+
+export default Bannerimg;
