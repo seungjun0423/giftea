5ce2ecca5367998a7c990a208f5cbc88be82d4f9
Chae Lin Shin <cls561@nyu.edu>
2023-05-12 00:16:46 +0900

    [FE-navbar] 병합 후 코드 정리 (navbar - shared 폴더로 이동 및 css 파일 추가)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Main from './components/Main/index';
-import 'styles/index.css';
+import Main from 'components/Main/index';
+import Navbar from 'components/Shared/Navbar';
 
-import './styles/index.css';
+import 'styles/index.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- front/src/components/Main/index.tsx
+++ front/src/components/Main/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Banner from './banner';
 import FundingWrapper from './FundingWrapper';
-import Navbar from './navbar';
 
 function Main() {
   return (
     <div className="index">
-      <Navbar />
       <Banner />
       <FundingWrapper />
     </div>
