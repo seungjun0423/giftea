f72bd5ef465b74d936c3889ee9043335559146fe
seungjun0423 <sibonum@gmail.com>
2023-05-16 17:07:50 +0900

    port env 환경변수로 변경

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
 async function main() {
+	const port = process.env.NODE_SERVER_PORT;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(port);
 }
 main();
