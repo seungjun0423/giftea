c3fdda7be35b12b105f5c3a495e955460868b688
seungjun0423 <sibonum@gmail.com>
2023-05-16 17:14:47 +0900

    ConfigService 사용

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function main() {
-	const port = process.env.NODE_SERVER_PORT;
+	
+	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const port = configService.get('NODE_SERVER_PORT');
 
-  const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
 main();
