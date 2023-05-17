02557fa9777c44d510b415a201cc1df92b10e382
seungjun0423 <sibonum@gmail.com>
2023-05-17 19:39:26 +0900

    app.module.ts 미들웨어 추가

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import config from 'src/config/config';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [ 
@@ -16,4 +17,10 @@ import config from 'src/config/config';
 	
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+		.apply(LoggerMiddleware)
+		.forRoutes('users')
+	}
+}
