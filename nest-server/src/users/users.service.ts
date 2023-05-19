538d3694f582874a723578b9fc5c4abe016b9815
seungjun0423 <sibonum@gmail.com>
2023-05-19 17:31:23 +0900

    회원 업데이트&&삭제 수정

--- nest-server/src/users/users.service.ts
+++ nest-server/src/users/users.service.ts
@@ -24,14 +24,33 @@ export class UsersService {
 
 	/** 회원정보 수정 */
 	async updateUser(id: number,name: string,gender: string,age: number,phone: number,email: string): Promise<User | null> {
-		return this.prismaService.user.update({ 
-			where: {id: Number(id)},
-			data:{ name: name, gender: gender, age: age, phone: phone, email: email}
-		});
+		// 회원정보 유무 판단
+		const existed = await this.findUser(id);
+		
+		// 회원정보가 있는 경우에만 업데이트 진행
+		if(existed){
+			// 업데이트
+				this.prismaService.user.update({ 
+				where: {id: Number(id)},
+				data:{ name: name, gender: gender, age: age, phone: phone, email: email}
+			});
+
+			return this.prismaService.user.findUnique({ where: {id: Number(id)} });
+		}
 	}
 
 	/** 단일 사용자 데이터 삭제 */
 	async deleteUser(id:number): Promise<User | null> {
-		return this.prismaService.user.delete({ where: {id: Number(id)} });
+		// 회원정보 유무 판단
+		const existed = await this.findUser(id);
+		
+		// 회원정보가 있는 경우에만 삭제 진행
+		if(existed){
+			// 삭제
+			this.prismaService.user.delete({ where: {id: Number(id)} });
+
+			// 삭제 확인
+			return this.prismaService.user.findUnique({ where: {id: Number(id)} });
+		}
 	}
 }
