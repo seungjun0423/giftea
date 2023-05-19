814e44124dc0f432ab8f7d912398aa8a167eb7c1
seungjun0423 <sibonum@gmail.com>
2023-05-19 17:06:00 +0900

    사용자 정보 수정 기능 추가

--- nest-server/src/users/users.controller.ts
+++ nest-server/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateUsersDto } from './CreateUsersDto';
 import { UsersService } from './users.service';
 import { User } from '@prisma';
@@ -6,6 +6,11 @@ import { User } from '@prisma';
 @Controller('api/v1/users')
 export class UsersController {
 	constructor(private usersService: UsersService){}
+	
+	@Post()
+	async addUser(@Body() data: User): Promise<User> {
+		return this.usersService.addUser(data);
+	}
 
 	@Get()
 	async findAllUsers(): Promise<User[]> {
@@ -15,6 +20,11 @@ export class UsersController {
 	@Get(':id')
 	async findUser(@Param('id') id: number): Promise<User | null> {
 		return this.usersService.findUser(id);
+	}
+
+	@Put()
+	async updateUser(@Param('id') id: number,@Body() data: User): Promise<User | null> {
+		return this.usersService.updateUser(id, data.name, data.gender, data.age, data.phone, data.email);
 	}
 
 	@Delete(':id')
--- nest-server/src/users/users.service.ts
+++ nest-server/src/users/users.service.ts
@@ -7,6 +7,11 @@ export class UsersService {
 	// 프리스마 의존성 주입
 	constructor(private prismaService: PrismaService){}
 
+	/** 사용자 추가 */
+	async addUser(data: User): Promise<User> {
+		return this.prismaService.user.create({data : data});
+	}
+
 	/** 전체 사용자 조회 */
 	async findAllUsers(): Promise<User[]> {
 		return this.prismaService.user.findMany();
@@ -14,13 +19,19 @@ export class UsersService {
 
 	/** 단일 사용자 조회 */
 	async findUser(id: number): Promise<User | null> {
-		return this.prismaService
-			.user.findUnique({ where: {id: Number(id)} });
+		return this.prismaService.user.findUnique({ where: {id: Number(id)} });
+	}
+
+	/** 회원정보 수정 */
+	async updateUser(id: number,name: string,gender: string,age: number,phone: number,email: string): Promise<User | null> {
+		return this.prismaService.user.update({ 
+			where: {id: Number(id)},
+			data:{ name: name, gender: gender, age: age, phone: phone, email: email}
+		});
 	}
 
 	/** 단일 사용자 데이터 삭제 */
 	async deleteUser(id:number): Promise<User | null> {
-		return this.prismaService
-		.user.delete({ where: {id: Number(id)} });
+		return this.prismaService.user.delete({ where: {id: Number(id)} });
 	}
 }
