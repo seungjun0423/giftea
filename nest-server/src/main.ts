aef44f2e523a61015cd0873f193b19ed2010b2d7
seungjun0423 <sibonum@gmail.com>
2023-05-24 22:05:26 +0900

    임시저장

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -4,9 +4,15 @@ import { ConfigService } from '@nestjs/config';
 
 async function main() {
 	
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {cors:true});
 	const configService = app.get(ConfigService);
 	const port = configService.get<string>('server.port');
+	
+	app.enableCors({
+		origin: 'http://localhost:3000',
+  	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  	credentials: true,
+	});
 
   await app.listen(port);
 }
