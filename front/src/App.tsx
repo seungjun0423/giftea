5784acb17b45a73533fd93f910acbf0974055077
Chae Lin Shin <cls561@nyu.edu>
2023-05-28 15:00:24 +0900

    [FE-router] 펀딩만들기, 프로필 라우터 추가 세팅

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,9 +4,11 @@ import { Route, Routes } from 'react-router-dom';
 import Main from 'components/Main/index';
 import Navbar from 'components/Shared/Navbar';
 import Funding from 'components/Funding';
+import FundingComplete from 'components/FundingComplete';
+import Profile from 'components/Profile';
+import MakeFunding from 'components/MakeFunding';
 
 import 'styles/index.css';
-import FundingComplete from 'components/FundingComplete';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/funding" element={<Funding/>} />
           <Route path="/complete" element={<FundingComplete />} />
+          <Route path="/my" element={<Profile />} />
+          <Route path="/create" element={<MakeFunding />} />
         </Routes>
     </>
   );
--- front/src/components/Shared/Navbar.tsx
+++ front/src/components/Shared/Navbar.tsx
@@ -9,20 +9,20 @@ function Navbar() {
   return (
     <nav>
       <div className="navbar_container">
-        <div className="title">GIFTEA</div>
+        <Link to="/" className="title">GIFTEA</Link>
         <div className="navbar_icon_container">
           <div className="navbar_icon">
             <img src={loginIcon} alt="login icon" className="navbar_icon_img" />
             <div className="navbar_icon_title">로그인</div>
           </div>
-          <div className="navbar_icon">
+          <Link to="/my" className="navbar_icon">
             <img src={mypageIcon} alt="mypage icon" className="navbar_icon_img" />
             <div className="navbar_icon_title">마이페이지</div>
-          </div>
-          <div className="navbar_icon">
+          </Link>
+          <Link to="/create" className="navbar_icon">
             <img src={makeFundingIcon} alt="make funding icon" className="navbar_icon_img make_funding_img" />
             <div className="navbar_icon_title make_funding_menu">펀딩 만들기</div>
-          </div>
+          </Link>
         </div>
       </div>
       <hr className="navbar_line" />
