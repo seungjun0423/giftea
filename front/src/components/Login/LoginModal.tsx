279722c7889f27bea8a7b688adbad295880a6662
seungjun0423 <sibonum@gmail.com>
2023-05-28 14:37:36 +0900

    LoginModal.tsx 반응형 작업 완료

--- front/src/components/Login/LoginModal.tsx
+++ front/src/components/Login/LoginModal.tsx
@@ -15,21 +15,25 @@ function LoginModal({isOpenLoginModal}: onOff) {
 		const data = await axios.get("http://localhost:4000/api/v1/users").then(result=>result.data);
 		return data;
 	}
+
 	console.log(test());
+
   return isOpenLoginModal ?(
 		<div className='login_modal'>
-			<div className='close_btn'>
-				x
+			<div className='close_btn_box'>
+				<button className='close_btn'>x</button>
 			</div>
 
 			<img className='login_logo' src={login_logo}>
 			</img>
 
-			<input className='input_box' type={'email'} placeholder={'이메일'}>
-			</input>
+			<div className='input_box_wrapper'>
+				<input className='input_box' type={'email'} placeholder={'이메일'}>
+				</input>
 
-			<input className='input_box' type={'password'} placeholder={'비밀번호'}>
-			</input>
+				<input className='input_box' type={'password'} placeholder={'비밀번호'}>
+				</input>
+			</div>
 
 			<button className='login_btn'>
 				로그인 하기
